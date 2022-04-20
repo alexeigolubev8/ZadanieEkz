e5e8aed7bc21b5210c67848449d60cb63ab2517c
kpc <kostroma.kpc@gmal.com>
2022-04-20 09:28:16 +0300

    ekzam

---
+++ Service/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class Application
+    {
+        private List<TaskList> taskLists;
+        public Application()
+        {
+           
+
+    }
+}
---
+++ Service/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class Task
+    {
+        ///<summary>
+        ///Название задачи
+        ///</summary>
+    public string Title { get; set; }
+        ///<summary>
+        ///Дата и время завершение выполнения
+        /// </summary> 
+     public DateTime Due { get; set; }
+    }
+}
